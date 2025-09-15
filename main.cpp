1100e90219835e03beb1a9573e2c75e962d0a3f3
Wake1st <joeltwakefield@gmail.com>
2025-09-15 15:36:42 -0600

    cleanup

--- main.cpp
+++ main.cpp
@@ -9,25 +9,6 @@
 #include "captioner.h"
 
 
-// Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"uniform float size;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(size * aPos.x, size * aPos.y, size * aPos.z, 1.0);\n"
-"}\0";
-
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"uniform vec4 color;\n"
-"void main()\n"
-"{\n"
-"   FragColor = color;\n"
-"}\n\0";
-
-
 void error_callback(int error, const char* description)
 {
 	fprintf(stderr, "Error: %s\n", description);
@@ -97,13 +78,6 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// set triangle vertices
-	GLfloat vertices[] = {
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		+0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		+0.0f, +0.5f * float(sqrt(3)) * 2 / 3, 0.0f
-	};
-
 	GLFWwindow* window = glfwCreateWindow(800, 800, "Custom Captions", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -131,53 +105,8 @@ int main() {
 	// run winow
 	glViewport(0, 0, 1000, 800);
 
-	// load shaders
-	GLuint vertextShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertextShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertextShader);
-
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertextShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glDeleteShader(vertextShader);
-	glDeleteShader(fragmentShader);
-
-	// buffers
-	GLuint VAO, VBO;
-
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	// triangle data
-	bool drawTriangle = false;
-
-	float size = 1.0f;
-	float color[4] = { 0.8f, 0.3f, 0.02f, 1.0f };
-
 	// caption data
 	Captioner* captioner = new Captioner();
-
-	// initialize triangle data
-	glUseProgram(shaderProgram);
-	glUniform1f(glGetUniformLocation(shaderProgram, "size"), size);
-	glUniform4f(glGetUniformLocation(shaderProgram, "color"), color[0], color[1], color[2], color[3]);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -187,13 +116,6 @@ int main() {
 		// set background color
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		// graphics
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-
-		if (drawTriangle)
-			glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// set new frame
 		ImGui_ImplOpenGL3_NewFrame();
@@ -205,11 +127,6 @@ int main() {
 
 		// tool
 		captioner->update();
-
-		// set triable data
-		glUseProgram(shaderProgram);
-		glUniform1f(glGetUniformLocation(shaderProgram, "size"), size);
-		glUniform4f(glGetUniformLocation(shaderProgram, "color"), color[0], color[1], color[2], color[3]);
 
 		// render imgui
 		ImGui::Render();
@@ -233,11 +150,6 @@ int main() {
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
-
-	// close buffers
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VAO);
-	glDeleteProgram(shaderProgram);
 
 	// close glfw window
 	glfwDestroyWindow(window);
